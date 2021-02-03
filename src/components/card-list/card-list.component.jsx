bc7332735158a66e58e2c8fa3c2b61e79fa1090b
ikornzaft <ikornzaft@gmail.com>
2021-02-03 05:54:34 -0300

    Styling card component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,10 +3,18 @@ import { Card } from '../card/card.component';
 
 import './card-list.styles.css';
 
+function selectKindOfPlace (name) {
+  let placeType = "generic";
+  if (name.toLowerCase().includes('museo')) placeType = "museum";
+  if (name.toLowerCase().includes('teatro')) placeType = "theatre";
+  if (name.toLowerCase().includes('plaza') || name.toLowerCase().includes('parque') || name.toLowerCase().includes('jardín')) placeType="park";
+  return `card-container ${placeType}`;
+};
+
 export const CardList = props => (
   <div className='card-list'>
     {props.places.map(place => (
-      <Card key={place.id} place={place}/>
+      <Card key={place.id} place={place} class={selectKindOfPlace(place.nombre)} />
     ))}
   </div>
 );
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import './card.styles.css';
 
 export const Card = props => (
-  <div className='card-container'>
+  <div className={props.class}>
     <h2> {props.place.nombre} </h2>
+    <div className='card__content'>
+      <div className='card__image'></div>
+    </div>
   </div>
 )
